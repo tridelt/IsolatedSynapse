b09d8ef4a732ef3b83c05b0cc50a4a548231c140
Luca <luca.rauchenberger@gmail.com>
2023-03-25 16:05:00 +0100

    chasing and animations

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -8,8 +8,13 @@ public class Enemy : MonoBehaviour
     public Transform player;
     public float moveSpeed;
     public int patrolDestination = 0;
-    public float enemySpeed = 2.5f;
+    public float enemySpeed = 50.5f;
     public float meleeDistance = 0.3f;
+    private Animator _anim;
+    Vector3 _previousPosition;
+    private bool _isFacingRight = false;
+    private bool _hasAggro = false;
+
 
     public Transform aggroCenter;
     // private Vector2 middlePatrolPoint;
@@ -29,9 +34,17 @@ public class Enemy : MonoBehaviour
     //      Gizmos.DrawSphere(middlePatrolPoint, 1f);
     //  }
 
+    void Start()
+    {
+        _anim= GetComponent<Animator>();
+        _previousPosition = transform.position;
+    }
+
 
     void Update()
     {
+        Debug.Log(_anim);
+
         /*
             every time reevaluate the state again
             --> puede ser {chasing, attacking, returning, patrolling}
@@ -40,6 +53,11 @@ public class Enemy : MonoBehaviour
 
         if (Vector2.Distance(player.position, aggroCenter.position) < 5f)
         {
+            _hasAggro = true;
+        }
+
+        if (_hasAggro)
+        {
             // Debug.Log("chasing enemey");
             // chase mode
             // check if in melee range
@@ -47,6 +65,12 @@ public class Enemy : MonoBehaviour
             {
                 transform.position =
                     Vector2.MoveTowards(transform.position, player.position, enemySpeed * Time.deltaTime);
+                _anim.SetBool("inMeleeRange", false);
+            }
+            else
+            {
+                Debug.Log(_anim);
+                _anim.SetBool("inMeleeRange", true);
             }
         }
         else
@@ -61,54 +85,20 @@ public class Enemy : MonoBehaviour
             }
         }
 
+        Vector3 movementDelta = transform.position - _previousPosition;
+        _previousPosition = transform.position;
 
-        // else {
-        //     if(patrolDestination == 0) {
-        //         // patrol
-        //         transform.position = Vector2.MoveTowards(transform.position, patrolPoints[0].position, speed * Time.deltaTime);
-        //         if(Vector2.Distance(transform.position, patrolPoints[0].position) < 0.1f) {
-        //             patrolDestination = 1;
-        //         }
-        //     } else if(patrolDestination == 1) {
-        //         // patrol
-        //         transform.position = Vector2.MoveTowards(transform.position, patrolPoints[1].position, speed * Time.deltaTime);
-        //         if(Vector2.Distance(transform.position, patrolPoints[1].position) < 0.1f) {
-        //             patrolDestination = 0;
-        //         }
-        //     }
-        // }
-
-
-        // if(patrolDestination == 0) {
-        //     // patrol
-        //     transform.position = Vector2.MoveTowards(transform.position, patrolPoints[0].position, speed * Time.deltaTime);
-        //     if(Vector2.Distance(transform.position, patrolPoints[0].position) < 0.1f) {
-        //         patrolDestination = 1;
-        //     }
-        // } else if(patrolDestination == 1) {
-        //     // patrol
-        //     transform.position = Vector2.MoveTowards(transform.position, patrolPoints[1].position, speed * Time.deltaTime);
-        //     if(Vector2.Distance(transform.position, patrolPoints[1].position) < 0.1f) {
-        //         patrolDestination = 0;
-        //     }
-        // }
-
-        // if(Vector2.Distance(transform.position, player.transform.position) < 5f)
-        // {
-        //     Debug.Log("CLOSE");
-        //     // chase
-        //     // transform.position = Vector2.MoveTowards(transform.position, player.transform.position, speed * Time.deltaTime);
-        // }
-        // else
-        // {
-        //        Debug.Log("FAR AWAY");
-        //     // patrol
-        //     // transform.position = Vector2.MoveTowards(transform.position, new Vector2(0, 0), speed * Time.deltaTime);
-        // }
-
-
-        // float deltaX = Input.GetAxis("Horizontal") * speed;
-        // Vector2 movement = new Vector2(_body.velocity.x, -speed);
-        // _body.velocity = movement;
+        if (movementDelta.x > 0 && !_isFacingRight)
+        {
+            transform.localScale = new Vector3(-Mathf.Sign(transform.localScale.x), transform.localScale.y, 1f);
+            _isFacingRight = true;
+        }
+        // Otherwise if the input is moving the player left and the player is facing right...
+        else if (movementDelta.x < 0 && _isFacingRight)
+        {
+            // ... flip the player.
+            transform.localScale = new Vector3(-Mathf.Sign(transform.localScale.x), transform.localScale.y, 1f);
+            _isFacingRight = false;
+        }
     }
 }
