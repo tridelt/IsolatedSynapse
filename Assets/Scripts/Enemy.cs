f4794bd1bf7cb4ac32b6168300408fea19a118ae
Luca <luca.rauchenberger@gmail.com>
2023-03-30 18:08:05 +0200

    enemy flashes red upon taking damage

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -24,13 +24,10 @@ public class Enemy : MonoBehaviour
     private bool _hasAggro = false;
     public SpriteRenderer _spriteRenderer;
 
-    // public bool isDead {
-    //     get
-    //     {
-    //         return current_health <= 0;
-    //         
-    //     }
-    // }
+    public bool isAlive
+    {
+        get { return current_health > 0; }
+    }
 
     public Transform aggroCenter;
 
@@ -53,77 +50,95 @@ public class Enemy : MonoBehaviour
     {
         // Debug.Log(transform.position.y - player.position.y);
 
-
-        if (transform.position.y < player.position.y)
+        if (isAlive)
         {
-            // Debug.Log(transform.position.y);
-            // Debug.Log(player.position.y);
-            // Debug.Log(transform.position.y < player.position.y);
-            _spriteRenderer.sortingOrder = 2;
-        }
-        else
-        {
-            _spriteRenderer.sortingOrder = 0;
-        }
-
-        if (Vector2.Distance(player.position, aggroCenter.position) < 5f)
-        {
-            _hasAggro = true;
-        }
-
-        if (_hasAggro)
-        {
-            if (Vector2.Distance(transform.position, player.position) > meleeDistance)
+            if (transform.position.y < player.position.y)
             {
-                transform.position =
-                    Vector2.MoveTowards(transform.position, player.position, enemySpeed * Time.deltaTime);
-                _anim.SetBool("inMeleeRange", false);
+                // Debug.Log(transform.position.y);
+                // Debug.Log(player.position.y);
+                // Debug.Log(transform.position.y < player.position.y);
+                _spriteRenderer.sortingOrder = 2;
             }
             else
             {
-                _anim.SetBool("inMeleeRange", true);
+                _spriteRenderer.sortingOrder = 0;
             }
-        }
-        else
-        {
-            transform.position = Vector2.MoveTowards(transform.position, patrolPoints[patrolDestination].position,
-                enemySpeed * Time.deltaTime);
-            if (Vector2.Distance(transform.position, patrolPoints[patrolDestination].position) < 0.1f)
+
+            if (Vector2.Distance(player.position, aggroCenter.position) < 5f)
             {
-                patrolDestination = (patrolDestination + 1) % patrolPoints.Length;
+                _hasAggro = true;
             }
-        }
 
-        Vector3 movementDelta = transform.position - _previousPosition;
-        _previousPosition = transform.position;
+            if (_hasAggro)
+            {
+                if (Vector2.Distance(transform.position, player.position) > meleeDistance)
+                {
+                    transform.position =
+                        Vector2.MoveTowards(transform.position, player.position, enemySpeed * Time.deltaTime);
+                    _anim.SetBool("inMeleeRange", false);
+                }
+                else
+                {
+                    _anim.SetBool("inMeleeRange", true);
+                }
+            }
+            else
+            {
+                transform.position = Vector2.MoveTowards(transform.position, patrolPoints[patrolDestination].position,
+                    enemySpeed * Time.deltaTime);
+                if (Vector2.Distance(transform.position, patrolPoints[patrolDestination].position) < 0.1f)
+                {
+                    patrolDestination = (patrolDestination + 1) % patrolPoints.Length;
+                }
+            }
 
-        if (movementDelta.x > 0 && !_isFacingRight)
-        {
-            transform.localScale = new Vector3(-Mathf.Sign(transform.localScale.x), transform.localScale.y, 1f);
-            _isFacingRight = true;
-        }
-        // Otherwise if the input is moving the player left and the player is facing right...
-        else if (movementDelta.x < 0 && _isFacingRight)
-        {
-            transform.localScale = new Vector3(-Mathf.Sign(transform.localScale.x), transform.localScale.y, 1f);
-            _isFacingRight = false;
+            Vector3 movementDelta = transform.position - _previousPosition;
+            _previousPosition = transform.position;
+
+            if (movementDelta.x > 0 && !_isFacingRight)
+            {
+                transform.localScale = new Vector3(-Mathf.Sign(transform.localScale.x), transform.localScale.y, 1f);
+                _isFacingRight = true;
+            }
+            // Otherwise if the input is moving the player left and the player is facing right...
+            else if (movementDelta.x < 0 && _isFacingRight)
+            {
+                transform.localScale = new Vector3(-Mathf.Sign(transform.localScale.x), transform.localScale.y, 1f);
+                _isFacingRight = false;
+            }
         }
     }
 
     public void TakeDamage(float attackDamage)
     {
+        if (!isAlive) return;
+        
         current_health -= attackDamage;
+        StartCoroutine(FlashRed());
+
         if (current_health <= 0)
         {
-            transform.position = transform.position;
+            _anim.SetBool("isDead", true);
             Invoke(nameof(EnemyDies), 5f);
-            // Destroy(gameObject);
         }
     }
 
     private void EnemyDies()
     {
-        _anim.SetBool("isDead", true);
         Destroy(gameObject, 1f);
+    }
+
+    private IEnumerator FlashRed()
+    {
+        yield return new WaitForSeconds(0.1f);
+
+        // Change the color of the sprite to red
+        _spriteRenderer.color = Color.red;
+
+        // Wait for 0.1 seconds
+        yield return new WaitForSeconds(0.3f);
+
+        // Change the color of the sprite back to white
+        _spriteRenderer.color = Color.white;
     }
 }
