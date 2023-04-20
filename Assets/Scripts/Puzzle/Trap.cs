862104694dc048cd886433bfa32bc43c99633a72
Luca <luca.rauchenberger@gmail.com>
2023-04-20 17:56:28 +0200

    added trap sound

--- Assets/Scripts/Puzzle/Trap.cs
+++ Assets/Scripts/Puzzle/Trap.cs
@@ -4,25 +4,27 @@ using UnityEngine;
 
 public class Trap : MonoBehaviour
 {
-    
+    public AudioClip trap;
+    AudioSource audio;
     public Transform blockade_east;
     public Transform blockade_south;
-    
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        audio = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
-    
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         blockade_east.gameObject.SetActive(true);
         blockade_south.gameObject.SetActive(true);
+        audio.clip = trap;
+        audio.Play();
     }
 }
