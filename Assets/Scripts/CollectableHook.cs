d4a9a5a0b31734607231bc3dbf68b0b303ed19e7
Felipe Najson <felipe.najson@gmail.com>
2023-04-24 21:29:23 +0200

    collectable key and hook finished

---
+++ Assets/Scripts/CollectableHook.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectableHook : MonoBehaviour
+{
+    public float amplitude = 0.2f;
+    private Vector3 initialPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        initialPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 newPosition = initialPosition;
+        newPosition.y += Mathf.Sin(Time.time) * amplitude;
+        transform.position = newPosition;
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            // other.GetComponent<PlayerController>().CollectHook();
+            Destroy(gameObject);
+        }
+    }
+}
